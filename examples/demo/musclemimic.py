469342b49bf866541086c80d56da1d230037afac
ttktjmt <tatsuki.tsujimoto@gmail.com>
2026-02-19 10:34:21 +0900

    Rename all muwanx into mjswan, Test and demo passed

--- examples/demo/musclemimic.py
+++ examples/demo/musclemimic.py
@@ -1,12 +1,12 @@
 import mujoco
 import musclemimic_models as mm
 
-import muwanx
+import mjswan
 
 myofullbody_path = mm.get_xml_path("myofullbody")
 myofullbody_mjspec = mujoco.MjSpec.from_file(str(myofullbody_path))
 
-builder = muwanx.Builder()
+builder = mjswan.Builder()
 
 mm_project = builder.add_project(name="MuscleMimic Demo")
 
