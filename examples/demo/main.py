740e1b033b11fec53a1a6afa11bb0c15b42ea5f9
ttktjmt <tatsuki.tsujimoto@gmail.com>
2026-03-01 10:41:16 +0900

    Force exit after build to avoid thread cleanup hang

--- examples/demo/main.py
+++ examples/demo/main.py
@@ -282,6 +282,9 @@ def main():
     app = builder.build()
     if os.getenv("MJSWAN_NO_LAUNCH") != "1":
         app.launch()
+    else:
+        # Force exit to prevent imported libraries　from hanging the process on non-daemon thread cleanup.
+        os._exit(0)
 
 
 if __name__ == "__main__":
